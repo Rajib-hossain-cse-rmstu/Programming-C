e0983edef97838642b61e9852494ba5aa0a11de4
Rajib-hossain-cse-rmstu <rajibraju771@gmail.com>
2022-11-03 01:50:33 +0600

    Simple Input output program

---
+++ Input Output Program Simple.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter 1st number: ");
+    scanf("%d", &a);
+    printf("\n");
+    printf("Enter 2nd number: ");
+    scanf("%d", &b);
+    printf("\n");
+
+    printf("Sum of 1st and 2nd Number : %d", a+b);
+}
